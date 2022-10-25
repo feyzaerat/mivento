8d9bbca267f5d6d34b87b691fa57c03a412ecd04
Feyza Karanfil Erat <feyzakrnfl@gmail.com>
2022-10-25 16:28:27 +0300

    created includes

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require 'config/Database.php';
     <?php require 'includes/head.php'; ?>
 </head>
 <body id="body-pd">
+
 <header class="header" id="header">
     <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
     <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""> </div>
